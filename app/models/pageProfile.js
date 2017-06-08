ec38d47ba05fe44369000901f52451455c385586
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-06-08 13:25:08 -0400

    modeling pageProfile + setting relations (fk) with page & profile

--- app/models/pageProfile.js
+++ app/models/pageProfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const db = require('../../db');
+const Profile = require('../models/profile');
+const Page = require('../models/page');
 
 const PageProfile = db.sequelize.define('pageProfile', {
     status: {
@@ -11,5 +13,8 @@ const PageProfile = db.sequelize.define('pageProfile', {
         }
     }
 });
+
+PageProfile.belongsTo(Profile, {foreignKey: 'profileId', targetKey: 'id'});
+PageProfile.belongsTo(Page, {foreignKey: 'pageId', targetKey: 'id'});
 
 module.exports = PageProfile;
